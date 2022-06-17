ae071bcd5eaa7455e6ab743930d7b70638799732
Nick Douglas <douglas.extern@schuettflix.de>
2022-06-17 15:54:06 +0100

    icon size change

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -8,7 +8,7 @@ export type TSkill = {
 }
 
 const Skill = ({ name, icon, hideName = false, size = 'lg' }: TSkill) => {
-  const iconSize = size === 'sm' ? 'h-7 w-7 md:w-8 md:h-8 lg:h-6 lg:w-6' : 'h-10 w-10 md:w-14 md:h-14 lg:h-12 lg:w-12';
+  const iconSize = size === 'sm' ? 'h-7 w-7 md:w-8 md:h-8 lg:h-7 lg:w-7' : 'h-10 w-10 md:w-14 md:h-14 lg:h-12 lg:w-12';
   return (
     <>
       <div className={`${iconSize} relative`}>
